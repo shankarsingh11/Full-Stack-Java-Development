683a28f4d61564e95ff7f72cf99dee3365eae223
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-10-03 17:12:37 +0530

    Exception handling

---
+++ JavaAdvanced/src/exceptionhandling/ExcepExm7.java
@@ -0,0 +1,38 @@
+package exceptionhandling;
+
+public class ExcepExm7 {
+	
+	public static void disp()
+	{
+		try
+		{
+			System.out.println("Inside Outer try");
+			try
+			{
+				System.out.println("Inside Inner try");
+				System.out.println(10/0);
+			}
+			catch (NullPointerException e)
+			{
+				System.out.println("Inside inner catch");
+			}
+			System.out.println("Outside inner try-catch");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Inside Outer catch");
+		}
+		finally
+		{
+			System.out.println("Inside Outer finally");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		disp();
+
+	}
+
+}
