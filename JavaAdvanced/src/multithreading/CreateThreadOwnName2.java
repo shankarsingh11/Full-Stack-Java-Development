c7016d08f0c4950798cadf6357763740f322c469
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-10-26 08:02:13 +0530

    create own name Thread

---
+++ JavaAdvanced/src/multithreading/CreateThreadOwnName2.java
@@ -0,0 +1,38 @@
+package multithreading;
+
+
+class MyRunnable3 implements Runnable
+{
+   @Override
+   public void run()
+   {   
+	 // Thread.currentThread().setName("vishnu");
+	  Thread.currentThread().setName("shankar");
+	   System.out.println("run() is executed by :: " + Thread.currentThread().getName());
+	   
+	   int[] a = new int[5];
+	   
+	   for(int i = 0;i<=5;i++) {
+		   System.out.println(a[i]);
+	   }
+	   
+   }
+}
+
+public class CreateThreadOwnName2 {
+	
+	public static void main(String[] args) {
+		
+		MyRunnable3 r = new MyRunnable3();
+		//Thread t = new Thread();
+		Thread t = new Thread(r);
+		
+		 t.start();
+		 
+    Thread.currentThread().setName("shankar");
+    System.out.println("main is executed by :: " + Thread.currentThread().getName());
+    System.out.println(10/0);
+    
+	}
+
+}
