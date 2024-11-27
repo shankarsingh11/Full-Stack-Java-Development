7b86c4c7aa4bdf81a32c3fd7b446b63386b4649e
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-11-27 13:23:18 +0530

    linkedlist commit

---
+++ JavaAdvanced/src/collection/LaunchLL.java
@@ -0,0 +1,30 @@
+package collection;
+
+import java.util.*;
+
+public class LaunchLL {
+
+	public static void main(String[] args) {
+		
+		LinkedList ll1 = new LinkedList();
+		
+		ll1.add(11);
+		ll1.add("shankar");
+		ll1.add('B');
+		ll1.add(2.5);
+		
+		System.out.println(ll1);
+		ll1.add(0,"hyder");
+		System.out.println(ll1);
+		ll1.add(3,"Benguluru");
+		System.out.println(ll1);
+		ll1.add(6,"New York");
+		System.out.println(ll1);
+		ll1.add(7,"America");
+		System.out.println(ll1);
+		
+		
+
+	}
+
+}
