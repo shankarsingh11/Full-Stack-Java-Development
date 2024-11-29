59f9faa1ddbbd1e16e3c58880a1331a998d6fd76
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-11-29 14:36:58 +0530

    add arraylist commit

--- JavaAdvanced/src/collection/LaunchAL1.java
+++ JavaAdvanced/src/collection/LaunchAL1.java
@@ -1,18 +1,21 @@
 package collection;
+
 // To use any of the collection classes we need to import
 import java.util.*;
 
 public class LaunchAL1 {
-
+           
 	public static void main(String[] args) {
 		
 		ArrayList al1 = new ArrayList();
+		
 		al1.add(10);
 		al1.add(20);
 		al1.add(30);
 		al1.add(40);
 		
 		// homogeneous type of data is allowed
+		
 		System.out.println(al1);
 		System.out.println("***********************");
 		
@@ -25,6 +28,7 @@ public class LaunchAL1 {
 		al2.add(45.87);
 		
 		// hetorogeneous type of data is allowed
+		
 		System.out.println(al2);
 		
 		System.out.println("******************");
@@ -40,6 +44,7 @@ public class LaunchAL1 {
 //		System.out.println("*************");
 		
 		al3.addAll(al2);
+		
 		System.out.println(al3);
 		
 		System.out.println("*****************");
