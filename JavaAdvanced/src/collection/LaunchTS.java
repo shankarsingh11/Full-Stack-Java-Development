c7a0a16faadebe72403ae12a5f07e8400a04d254
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-11-29 14:37:19 +0530

    add TreeSet commit

---
+++ JavaAdvanced/src/collection/LaunchTS.java
@@ -0,0 +1,37 @@
+package collection;
+
+import java.util.*;
+
+public class LaunchTS {
+
+	public static void main(String[] args) {
+		
+        TreeSet ts = new TreeSet();
+        ts.add(20);
+        ts.add(10);
+        ts.add(25);
+        ts.add(100);
+        ts.add(30);
+        ts.add(50);
+        ts.add(15);
+        System.out.println(ts);
+        System.out.println("***********");
+        
+        System.out.println(ts.ceiling(20));
+        System.out.println(ts.higher(20));
+        System.out.println(ts.floor(20));
+        System.out.println(ts.lower(20));
+        
+        System.out.println("**************");
+        
+        System.out.println(ts.ceiling(35));
+        System.out.println(ts.higher(35));
+        System.out.println(ts.floor(35));
+        System.out.println(ts.lower(35));
+        
+       
+        
+        
+	}
+
+}
