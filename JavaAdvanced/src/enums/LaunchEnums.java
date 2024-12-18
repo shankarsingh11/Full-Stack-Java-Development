49695b2d89f306f0c0a601b020670ea1d28362f3
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-12-18 19:07:25 +0530

    enum commit

---
+++ JavaAdvanced/src/enums/LaunchEnums.java
@@ -0,0 +1,43 @@
+package enums;
+
+enum Results{
+	 PASS , FAIL , NR; // static final by default
+	// Fields ==> insatnce variable ==> properties
+	//methods
+	//constructor
+	
+	
+}
+
+//enum Gender{
+//	MALE , FEMALE , OTHER;
+//}
+
+//enum Compass{
+//	NORTH , SOUTH , EAST , WEST;
+//}
+
+// separate .class for every enum 
+
+
+//class Demo{
+//	
+//	final int PASS = 35;
+////	PASS-->Error
+//	
+//}
+
+
+public class LaunchEnums {
+	
+	enum Gender{
+		MALE , FEMALE , OTHERS;
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
