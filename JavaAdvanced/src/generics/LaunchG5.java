cc2d11a5b2283414c361c10fc346cba050259e5b
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-12-28 21:43:33 +0530

    add generics commit

--- JavaAdvanced/src/generics/LaunchG5.java
+++ JavaAdvanced/src/generics/LaunchG5.java
@@ -6,7 +6,11 @@ public class LaunchG5 {
 
 	public static void main(String[] args) {
 		
+		// All of them same 
+		// Generics code 
         ArrayList<Integer> al = new ArrayList();
+ //       ArrayList<Integer> al = new ArrayList<>();
+  //      ArrayList<Integer> al = new ArrayList<Integer>();
         
         al.add(10);
         al.add(100);
